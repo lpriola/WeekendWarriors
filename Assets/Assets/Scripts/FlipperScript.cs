8481d3592fffec7bf49a9a554fc1c95e095cdadf
jtorres107 <55066863+jtorres107@users.noreply.github.com>
2019-10-29 23:11:26 -0400

    Scripts Version
    Scrips for flipper, plunger, and reset ball all work. Remember to use Unity version 2018.4.11f1. I tried to commit the entire file, but it kept giving me a Warning error about "LF will be replaced with CRLF." I'm assuming Deborah is using a Mac. In windows a line is represented with a carriage return (CR) and a line feed (LF) thus (CRLF). when you get code from git that was uploaded from a unix system they will only have an LF. So I just attached the Asset folder.

---
+++ Assets/Assets/Scripts/FlipperScript.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipperScript : MonoBehaviour
+{
+    public float rest_position = 0f;
+    public float left_pressed_position = -45f;   // position of left flipper when touched
+    public float right_pressed_position = 45f;   // position of right flipper when touched
+    public float strength_hit = 10000f;
+    public float flipper_damper = 150f;
+    HingeJoint hinge;
+    //public string input_name;
+    private float ScreenWidth;
+
+    void Start()
+    {
+        ScreenWidth = Screen.width;
+        hinge = GetComponent<HingeJoint>();
+        hinge.useSpring = true;
+    }
+    
+    void Update()
+    {
+        int i = 0;
+        JointSpring spring = new JointSpring();
+        spring.spring = strength_hit;
+        spring.damper = flipper_damper;
+
+        // keeps flipper at rest postion
+        spring.targetPosition = rest_position;
+        hinge.spring = spring;
+        hinge.useLimits = true;
+        while (i < Input.touchCount)
+        {
+            // activates right flipper if right screen is touched
+            if (Input.GetTouch(i).position.x > ScreenWidth / 2)
+            {
+                spring.targetPosition = right_pressed_position;
+                hinge.spring = spring;
+                hinge.useLimits = true;
+
+            }
+
+            // activates left flipper if left screen is touched
+            if (Input.GetTouch(i).position.x < ScreenWidth / 2)
+            {
+                spring.targetPosition = left_pressed_position;
+                hinge.spring = spring;
+                hinge.useLimits = true;
+            }
+            ++i;
+
+        }
+    }
+
+    /***********************************************/
+    /************** Uses Keyboard ******************/
+    /***********************************************/
+
+    /*
+    void Update()
+    {
+        JointSpring spring = new JointSpring();
+        spring.spring = strength_hit;
+        spring.damper = flipper_damper;
+
+        if (Input.GetAxis(input_name) == 1)
+        {
+            spring.targetPosition = pressed_position;
+        }
+
+        else
+        {
+            spring.targetPosition = rest_position;
+        }
+        hinge.spring = spring;
+        hinge.useLimits = true;
+    }
+    */
+}
---
+++ Assets/Assets/Scripts/touchLocation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class touchLocation 
+{
+    public int touchID;
+    public GameObject flipper;
+
+    public touchLocation(int newTouchID, GameObject newflipper)
+    {
+        touchID = newTouchID;
+        flipper = newflipper; 
+    }
+
+    
+}
