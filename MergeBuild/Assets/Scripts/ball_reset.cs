f547ed2141c3f3f63cd4612aa578e4a1df66a1d6
constantine946 <constantinelogvin@yahoo.com>
2019-11-07 22:35:17 -0500

    Update ball_reset.cs
    Cleaned up the code a little and simplified it for ball reset

--- MergeBuild/Assets/Scripts/ball_reset.cs
+++ MergeBuild/Assets/Scripts/ball_reset.cs
@@ -4,28 +4,19 @@ using UnityEngine;
 
 public class ball_reset : MonoBehaviour
 {
+    public GameObject pinball;
     private Vector3 initialPosition;
-    private Vector3 resetPosition;
-    private Quaternion initialRotation;
-    private float distance = 0.75f;  // change this depending on how "close" it needs to be
-
+   
     void Start()
     {
         initialPosition = transform.position;
-        //initialRotation = transform.rotation;
-        resetPosition = new Vector3(0,0,-7);
     }
 
     void Update()
     {
-        // Move the object upwards??
-        //transform.Translate(Vector3.up * Time.deltaTime);
-
-        // Reset the position if we are sufficiently close to (0,0,0)
-        if (Vector3.Distance(transform.position, resetPosition) < distance)
-        { 
-            transform.position = initialPosition;
+        if(pinball.transform.position.z < (-7.0))
+        {
+            pinball.transform.position = initialPosition;
         }
-
     }
 }
