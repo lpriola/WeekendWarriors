9e4bebb152c880668e200e05ec1c04324eb675e9
constantine946 <constantinelogvin@yahoo.com>
2019-11-24 15:44:43 -0500

    Update ball_reset.cs
    cleaned up the code

--- ball_reset.cs
+++ ball_reset.cs
@@ -1,31 +1,27 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ball_reset : MonoBehaviour
 {
+    public GameObject pinball;
     private Vector3 initialPosition;
-    private Vector3 resetPosition;
-    private Quaternion initialRotation;
-    private float distance = 0.75f;  // change this depending on how "close" it needs to be
-
+   
     void Start()
     {
         initialPosition = transform.position;
-        //initialRotation = transform.rotation;
-        resetPosition = new Vector3(0,0,-7);
     }
 
     void Update()
     {
-        // Move the object upwards??
-        //transform.Translate(Vector3.up * Time.deltaTime);
-
-        // Reset the position if we are sufficiently close to (0,0,0)
-        if (Vector3.Distance(transform.position, resetPosition) < distance)
-        { 
-            transform.position = initialPosition;
+        if((pinball.transform.position.z < (-7.0)) && (PlayerController.ballCount > 1))
+        {
+            pinball.transform.position = initialPosition;
+            PlayerController.ballCount -= 1;
         }
-
+        if (PlayerController.ballCount == 0)
+        {
+            DestroyImmediate(pinball);
+        }
     }
 }
