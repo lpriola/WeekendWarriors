025d377b303cabfb02712121af6c17f0e4c010ce
constantine946 <constantinelogvin@yahoo.com>
2019-11-24 16:15:36 -0500

    Add files via upload
    updated versions

---
+++ BonusRound.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BonusRound : MonoBehaviour
+{
+    public GameObject pinball;
+    public GameObject clone;
+    private Vector3 appearPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        appearPosition = new Vector3(3.0f, 0.15f, -4.35f);
+    }
+
+    // When enter black hole, extra ball created
+    private void OnTriggerEnter(Collider other)
+    {
+        pinball.transform.position = new Vector3((float)-2.0, (float)0.1, (float)4.7);
+        if (other.gameObject.CompareTag("Ball"))
+        {
+            clone = Instantiate(pinball);
+            clone.transform.position = appearPosition;
+            PlayerController.ballCount += 1;        // add an extra ball to counter
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(clone != null)
+        {
+            if (clone.transform.position.z <= (-6.0))
+            {
+                DestroyImmediate(clone);        //destroy the extra ball (clone)
+                PlayerController.ballCount -= 1;    // subtract one from ball counter
+            }
+        }
+    }
+}
---
+++ ball_reset.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball_reset : MonoBehaviour
+{
+    public GameObject pinball;
+    private Vector3 initialPosition;
+   
+    void Start()
+    {
+        initialPosition = transform.position;
+    }
+
+    void Update()
+    {
+        if((pinball.transform.position.z < (-7.0)) && (PlayerController.ballCount > 1))
+        {
+            pinball.transform.position = initialPosition;
+            PlayerController.ballCount -= 1;
+        }
+        if (PlayerController.ballCount == 0)
+        {
+            DestroyImmediate(pinball);
+        }
+    }
+}
