5481eb735249312633706b88fe0b6d8370f72902
dalalou <dla11g@my.fsu.edu>
2019-11-24 22:00:53 -0500

    Final Model
    Added to model, added scripts for bumpers and indicators, added "glass" lid, merged in camera scripts and bonus round script after modifying, add UI elements to hook to camera scripts.

---
+++ ModelFinishBuild/Assets/Scripts/ball_reset.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball_reset : MonoBehaviour
+{
+    private Vector3 initialPosition;
+    private Vector3 resetPosition;
+    private Quaternion initialRotation;
+    private float distance = 0.75f;  // change this depending on how "close" it needs to be
+
+    void Start()
+    {
+        initialPosition = transform.position;
+        //initialRotation = transform.rotation;
+        resetPosition = new Vector3(0,0,-7);
+    }
+
+    void Update()
+    {
+        // Move the object upwards??
+        //transform.Translate(Vector3.up * Time.deltaTime);
+
+        // Reset the position if we are sufficiently close to (0,0,0)
+        if (Vector3.Distance(transform.position, resetPosition) < distance)
+        { 
+            transform.position = initialPosition;
+        }
+
+    }
+}
